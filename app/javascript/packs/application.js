0af58a0c64e0405c0846ac549e598649bc82a085
gabrielle simon <gabriellesimon23@gmail.com>
2019-09-02 17:12:53 +0300

    added projects header, link in menu tab, need to freeze so that title doesn;t go into navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,4 +3,5 @@ import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
 import { navSticky } from '../components/navsticky';
 navSticky();
-
+import { effectSeven } from '../components/effect7';
+effectSeven();
